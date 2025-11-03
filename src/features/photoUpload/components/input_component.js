47fd6f5ce2d3894963b096b94975e367af0081f4
Mayur Madhwani <mayur07582@gmail.com>
2025-11-03 09:52:17 +0530

    input stylized

--- src/features/photoUpload/components/input_component.js
+++ src/features/photoUpload/components/input_component.js
@@ -1,21 +1,44 @@
 import styled from "styled-components";
+import { useSelector } from "react-redux";
+import { solidLight, solidDark } from "../../../colors/colors";
 
-import profileIcon from "../../../assets/icons/profile.png";
+const InputComponent = ({ icon, placeholder, onChange, type }) => {
+  const darkMode = useSelector((state) => state.darkMode.value);
 
-const InputComponent = ({icon, placeholder, onChange}) => {
   return (
-    <div>
-      <Image src={profileIcon}/>
-      <Input type="text" placeholder="Username"  onChange={(value) => console.log(value.target.value)}/>
-    </div>
+    <Main>
+      <Image src={icon} />
+      <Input
+        type={type}
+        placeholder={placeholder}
+        onChange={onChange}
+        maxLength={20}
+        style={{
+          borderColor: solidLight,
+        }}
+      />
+    </Main>
   );
 };
+
+const Main = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const Image = styled.img`
   height: 30px;
   width: 30px;
+  margin-right: 10px;
 `;
 
-const Input = styled.input``;
+const Input = styled.input`
+  border-radius: 20px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-width: 1px;
+  /* border-color: gray; */
+`;
 
 export default InputComponent;
